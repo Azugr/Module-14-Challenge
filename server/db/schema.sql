fac0a649b856e6f23a7e2809278e7f2c20592f1a
Azu <azugr@outlook.com>
2025-03-02 15:16:46 -0600

    DATABASE URL

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,16 +6,14 @@ CREATE DATABASE kanban_db;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
--- Create a new table
 CREATE TABLE tickets (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    status VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL CHECK (status IN ('Todo', 'In Progress', 'Done')),
     description TEXT,
-    assignedUserId INT,
-    FOREIGN KEY (assignedUserId) REFERENCES Users(id)
+    assignedUserId INTEGER REFERENCES users(id) ON DELETE SET NULL
 );
--- server/src/seeds/ticketSeeds.ts
+++ server/src/seeds/ticketSeeds.ts
@@ -1,37 +1,35 @@
 import { Ticket } from '../models/ticket.js';
 
 export const ticketSeeds = async () => {
+
+  await Ticket.destroy({ where: {} });
+
   await Ticket.bulkCreate([
     {
-      id: 1, 
       name: 'Create Homepage Layout',
-      status: 'In Progress', 
+      status: 'In Progress',
       description: 'Develop initial sketches and design prototypes for the homepage.',
       assignedUserId: 1
     },
     {
-      id: 2,
       name: 'Configure Repository',
-      status: 'Done', 
+      status: 'Done',
       description: 'Establish a new GitHub repository and include an introductory README.',
       assignedUserId: 2
     },
     {
-      id: 3,
       name: 'Build Authentication System',
       status: 'Todo',
       description: 'Create a secure login mechanism using token-based authentication.',
       assignedUserId: 3
     },
     {
-      id: 4,
       name: 'Validate API Endpoints',
       status: 'Todo',
       description: 'Perform comprehensive testing on API endpoints to ensure reliability.',
       assignedUserId: 4
     },
     {
-      id: 5,
       name: 'Launch Application',
       status: 'Todo',
       description: 'Deploy the final version of the application to the production environment.',
