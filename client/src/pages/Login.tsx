dcd48c62c4c265ffec0b64f93b611c692f2c87d4
Azu <azugr@outlook.com>
2025-03-03 10:15:03 -0600

    Corrected server

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,20 +1,23 @@
-import { useState, FormEvent, ChangeEvent } from "react";
-import Auth from '../utils/auth.js';
-import { login } from "../api/authAPI.js";
+import { useState, type FormEvent, type ChangeEvent } from 'react';
+import Auth from '../utils/auth';
+import { login } from '../api/authAPI';
+import type { UserLogin } from '../interfaces/UserLogin';
 
 const Login = () => {
-  const [loginData, setLoginData] = useState({
+  const [loginData, setLoginData] = useState<UserLogin>({
     username: '',
-    password: ''
+    password: '',
   });
 
   const [error, setError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -31,25 +34,35 @@ const Login = () => {
   };
 
   return (
-    <div className='container'>
-      <form className='form' onSubmit={handleSubmit}>
+    <div className='form-container'>
+      <form className='form login-form' onSubmit={handleSubmit}>
         <h1>Login</h1>
         {error && <p style={{ color: 'red' }}>{error}</p>}
-        <label>Username</label>
-        <input 
-          type='text'
-          name='username'
-          value={loginData.username || ''}
-          onChange={handleChange}
-        />
-        <label>Password</label>
-        <input 
-          type='password'
-          name='password'
-          value={loginData.password || ''}
-          onChange={handleChange}
-        />
-        <button type='submit'>Submit Form</button>
+        <div className='form-group'>
+          <label>Username</label>
+          <input
+            className='form-input'
+            type='text'
+            name='username'
+            value={loginData.username || ''}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            value={loginData.password || ''}
+            onChange={handleChange}
+          />
+        </div>
+        <div className='form-group'>
+          <button className='btn btn-primary' type='submit'>
+            Login
+          </button>
+        </div>
       </form>
     </div>
   );
